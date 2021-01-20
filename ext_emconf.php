106c92b3da9beafcf0b6a2feb73d120427566e8a
Paul Hansen <paul+typo3-sci@ecopixel.com>
2021-01-20 16:07:00 -0500

    [FEATURE] Add extension declaration file and icon.

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Page',
+    'description' => 'New page module interface.',
+    'category' => 'module',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => 0,
+    'author' => 'TYPO3 Structured Content Initiative Team',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-11.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'autoload' => [
+       'psr-4' => [
+          'TYPO3\\CMS\\Page\\' => 'Classes',
+       ],
+    ],
+];
