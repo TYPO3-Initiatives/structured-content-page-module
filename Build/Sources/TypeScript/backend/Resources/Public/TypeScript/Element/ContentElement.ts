8afe08c1892a55d7989a81d4cfad24c517090598
Jonas Eberle <flightvision@googlemail.com>
2021-03-20 17:03:07 +0100

    [TASK][WIP] use LitElement for CEs

--- Build/Sources/TypeScript/backend/Resources/Public/TypeScript/Element/ContentElement.ts
+++ Build/Sources/TypeScript/backend/Resources/Public/TypeScript/Element/ContentElement.ts
@@ -27,36 +27,57 @@ import 'TYPO3/CMS/Backend/Element/IconElement';
  */
 @customElement('typo3-backend-content-element')
 export class ContentElement extends LitElement {
-  @property({type: String}) type: string;
   @property({type: String}) header: string;
   @property({type: String}) content: string;
+  @property({type: String}) footer: string;
+  @property({type: Array}) actions: Array<any>;
+  @property({type: String, attribute: 'wrapper-class-name'}) wrapperClassName: string;
+  @property({type: String}) table: string;
+  @property({type: Number}) uid: string;
+  @property({type: Number, attribute: 'language-uid'}) languageUid: string;
+  @property({type: Number, attribute: 'unique-uid'}) uniqueId: string;
+  @property({type: Boolean}) disabled: string;
+  @property({type: Boolean}) show: string;
+  @property({type: Boolean}) versioned: string;
 
-  public static get styles(): CSSResult
-  {
-    return css`
-      :host {
-        display: block;
-        border: 1px solid var(--typo3-border-color, #ccc);
-        padding: 2em;
-      }
+  // public static get styles(): CSSResult
+  // {
+  //   return css`
+  //     :host {
+  //       display: block;
+  //       border: 1px solid var(--typo3-border-color);
+  //       padding: 2em;
+  //     }
+  //
+  //     :host([disabled]) {
+  //       border: 1px dashed var(--typo3-border-color);
+  //     }
+  //   `;
+  // }
 
-      :host([disabled]) {
-        border: 1px dashed var(--typo3-border-color, #ccc);
-      }
-    `;
+  public createRenderRoot(): HTMLElement | ShadowRoot {
+    return this;
   }
 
-  //public createRenderRoot(): HTMLElement | ShadowRoot {
-  //  return this;
-  //}
-
   public render(): TemplateResult {
+    // <typo3-backend-icon @click=${() => this.dispatchEvent(new CustomEvent('content-element:add'))}
+    //   identifier="actions-add" size="small"></typo3-backend-icon>
+    // </div>
     return html`
-      <div class="content-element">
-        <h2>${this.header}</h2>
-        ${unsafeHTML(this.content)}
-
-        <typo3-backend-icon @click=${() => this.dispatchEvent(new CustomEvent('content-element:add'))} identifier="actions-add" size="small"></typo3-backend-icon>
+      <div class="t3-page-ce ${this.wrapperClassName} t3js-page-ce t3js-page-ce-sortable"
+        id="element-tt_content-${this.uid}" data-table="tt_content" data-uid="${this.uid}"
+        data-language-uid="${this.languageUid}" style="${this.show? '': 'display: none'}">
+        <div class="t3-page-ce-dragitem" id="${this.uniqueId}">
+          ${unsafeHTML(JSON.parse(this.header))}
+          <div class="t3-page-ce-body">
+            <div class="t3-page-ce-body-inner">
+              <div class="${this.versioned ? 'ver-element': ''}">
+                ${unsafeHTML(JSON.parse(this.content))}
+              </div>
+            </div>
+            ${unsafeHTML(JSON.parse(this.footer))}
+          </div>
+        </div>
       </div>
     `;
   }
--- typo3/sysext/backend/Resources/Public/JavaScript/Element/ContentElement.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/Element/ContentElement.js
@@ -10,21 +10,20 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
-var __decorate=this&&this.__decorate||function(e,t,o,n){var r,c=arguments.length,d=c<3?t:null===n?n=Object.getOwnPropertyDescriptor(t,o):n;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)d=Reflect.decorate(e,t,o,n);else for(var i=e.length-1;i>=0;i--)(r=e[i])&&(d=(c<3?r(d):c>3?r(t,o,d):r(t,o))||d);return c>3&&d&&Object.defineProperty(t,o,d),d};define(["require","exports","lit-element","lit-html/directives/unsafe-html","TYPO3/CMS/Backend/Element/IconElement"],(function(e,t,o,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.ContentElement=void 0;let r=class extends o.LitElement{static get styles(){return o.css`
-      :host {
-        display: block;
-        border: 1px solid var(--typo3-border-color, #ccc);
-        padding: 2em;
-      }
-
-      :host([disabled]) {
-        border: 1px dashed var(--typo3-border-color, #ccc);
-      }
-    `}render(){return o.html`
-      <div class="content-element">
-        <h2>${this.header}</h2>
-        ${n.unsafeHTML(this.content)}
-
-        <typo3-backend-icon @click=${()=>this.dispatchEvent(new CustomEvent("content-element:add"))} identifier="actions-add" size="small"></typo3-backend-icon>
+var __decorate=this&&this.__decorate||function(e,t,r,o){var p,a=arguments.length,i=a<3?t:null===o?o=Object.getOwnPropertyDescriptor(t,r):o;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)i=Reflect.decorate(e,t,r,o);else for(var d=e.length-1;d>=0;d--)(p=e[d])&&(i=(a<3?p(i):a>3?p(t,r,i):p(t,r))||i);return a>3&&i&&Object.defineProperty(t,r,i),i};define(["require","exports","lit-element","lit-html/directives/unsafe-html","TYPO3/CMS/Backend/Element/IconElement"],(function(e,t,r,o){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.ContentElement=void 0;let p=class extends r.LitElement{createRenderRoot(){return this}render(){return r.html`
+      <div class="t3-page-ce ${this.wrapperClassName} t3js-page-ce t3js-page-ce-sortable"
+        id="element-tt_content-${this.uid}" data-table="tt_content" data-uid="${this.uid}"
+        data-language-uid="${this.languageUid}" style="${this.show?"":"display: none"}">
+        <div class="t3-page-ce-dragitem" id="${this.uniqueId}">
+          ${o.unsafeHTML(JSON.parse(this.header))}
+          <div class="t3-page-ce-body">
+            <div class="t3-page-ce-body-inner">
+              <div class="${this.versioned?"ver-element":""}">
+                ${o.unsafeHTML(JSON.parse(this.content))}
+              </div>
+            </div>
+            ${o.unsafeHTML(JSON.parse(this.footer))}
+          </div>
+        </div>
       </div>
-    `}};__decorate([o.property({type:String})],r.prototype,"type",void 0),__decorate([o.property({type:String})],r.prototype,"header",void 0),__decorate([o.property({type:String})],r.prototype,"content",void 0),r=__decorate([o.customElement("typo3-backend-content-element")],r),t.ContentElement=r}));
+    `}};__decorate([r.property({type:String})],p.prototype,"header",void 0),__decorate([r.property({type:String})],p.prototype,"content",void 0),__decorate([r.property({type:String})],p.prototype,"footer",void 0),__decorate([r.property({type:Array})],p.prototype,"actions",void 0),__decorate([r.property({type:String,attribute:"wrapper-class-name"})],p.prototype,"wrapperClassName",void 0),__decorate([r.property({type:String})],p.prototype,"table",void 0),__decorate([r.property({type:Number})],p.prototype,"uid",void 0),__decorate([r.property({type:Number,attribute:"language-uid"})],p.prototype,"languageUid",void 0),__decorate([r.property({type:Number,attribute:"unique-uid"})],p.prototype,"uniqueId",void 0),__decorate([r.property({type:Boolean})],p.prototype,"disabled",void 0),__decorate([r.property({type:Boolean})],p.prototype,"show",void 0),__decorate([r.property({type:Boolean})],p.prototype,"versioned",void 0),p=__decorate([r.customElement("typo3-backend-content-element")],p),t.ContentElement=p}));
